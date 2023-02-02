a652e6264d72d08250e3813ab2e101be39600434
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-02-02 20:51:36 -0300

    chore: Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
@@ -19,5 +19,5 @@ export function App() {
         iure nemo voluptatem!
       </p>
     </ThemeProvider>
-  );
+  )
 }
