eb79e89be29b2aeac5eb00ccffef028a97949abc
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-02-02 21:08:06 -0300

    feat: Add basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maiores
-        voluptatibus commodi odit laboriosam quia quos quis ipsam accusantium,
-        necessitatibus velit obcaecati, quidem molestiae doloribus! Odio ratione
-        iure nemo voluptatem!
-      </p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
